fea69d730dd71af4f88bd8bf597bd903dd0c792f
DShanShan <1750035283@qq.com>
2020-01-16 18:10:58 +0800

    Initial commit

---
+++ js/js.js
@@ -0,0 +1,96 @@
+/* // 两个变量交换数字
+        var num1=10;
+        var num2=20;
+        //var temp;
+        // temp=num1;
+        // num1=num2;
+        // num2=temp;
+        // console.log(num1,num2);
+
+        num1=num1+num2;
+        num2=num1-num2;
+        num1=num1-num2;
+        console.log(num1,num2);*/
+
+/*var str1 = parseInt(prompt("老板，请输入基本工资"));
+var str2 = parseInt(prompt("老板，还有奖金"));
+var num = str1+str2;
+console.log("总工资为"+num);*/
+
+// var num = 10.93;
+// console.log(Number(num));
+// console.log(parseInt(num));
+// console.log(parseFloat(num));
+// console.log(String(num));
+// console.log(Boolean("123"));
+// console.log(Boolean(""));
+// console.log(Boolean(NaN));
+// console.log(Boolean(undefined));
+// console.log(Boolean(0));
+// console.log(Boolean(1));
+
+/*
+
+
+var age=18;
+if(age>=18){
+    console.log("可以谈恋爱");
+}else {
+    console.log("好好学习，多敲代码");
+}
+
+var num1=13;
+var num2=8;
+if(num1>num2){
+    console.log("最大值为"+num1);
+}else {
+    console.log("最大值为"+num2);
+}
+
+var num3 = 12;
+if(num3%2==0){
+    console.log(num3+"是偶数");
+}else {
+    console.log(num3+"是奇数");
+}
+
+
+// 水仙花数
+
+
+var week=5;
+switch (week) {
+    case 1:
+        console.log("星期一");
+        break;
+    case 2:
+        console.log("星期二");
+        break;
+    case 3:
+        console.log("星期三");
+        break;
+    case 4:
+        console.log("星期四");
+        break;
+    case 5:
+        console.log("星期五");
+        break;
+    case 6:
+        console.log("星期六");
+        break;
+    case 7:
+        console.log("星期天");
+        break;
+    default :
+        console.log("你输入的星期有误");
+        break;
+}
+*/
+
+// for(var i=0;i<7;i++){
+//     console.log("跑步"+(i+1));
+// }
+
+
+
+
